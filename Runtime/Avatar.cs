5615e9d815085d62a894de0efc6120fde066e4ca
Bart Heijltjes <bart@wispfire.com>
2019-11-20 16:21:49 +0100

    Set up directory structure

---
+++ Runtime/Avatar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SMILEI.Core.Data
+{
+    /// <summary>
+    /// Represents a person with an emotional state.
+    /// </summary>
+    public class Avatar : MonoBehaviour
+    {
+        
+    }
+
+}
+
