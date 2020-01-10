ea5b67423e49f992e02843134e6c8abd290c075f
Bart Heijltjes <b.heijltjes@fontys.nl>
2020-01-10 15:14:21 +0100

    Moved DrawGraph to core

---
+++ Samples/MixerToAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SMILEI.Core;
+using UnityEngine;
+
+namespace SMILEI.Samples
+{
+    
+    public class MixerToAnimator : MonoBehaviour
+    {
+        public EmotionMixerAsset Mixer;
+        public Animator Animator;
+        public string AnimatorKey;
+
+        void Update()
+        {
+            Animator.SetFloat(AnimatorKey, Mixer.GetValue().Value);
+        }
+    }
+}
+
