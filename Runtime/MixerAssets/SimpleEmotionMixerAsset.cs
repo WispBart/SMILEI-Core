8e1af584c9346c7f9c55edc89f40c01699e73935
Bart Heijltjes <b.heijltjes@fontys.nl>
2020-01-30 17:09:37 +0100

    Added SimpleEmotionMixerAsset

---
+++ Runtime/MixerAssets/SimpleEmotionMixerAsset.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SMILEI.Core;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="SMILEI/Simple Emotion Mixer")]
+public class SimpleEmotionMixerAsset : EmotionMixerAsset
+{
+    [SerializeField] private SimpleEmotionMixer _implementation = new SimpleEmotionMixer();
+    public override IEmotionMixer Implementation => _implementation;
+
+    public void SetValue(float value, float confidence, float timestamp) =>
+        _implementation.SetValue(value, confidence, timestamp);
+}
+
+[Serializable] public class SimpleEmotionMixer : IEmotionMixer
+{
+    public float Timestamp;
+    public float Value;
+    public float Confidence;
+
+    public void SetValue(float value, float confidence, float timestamp)
+    {
+        this.Value = value;
+        this.Timestamp = timestamp;
+        this.Confidence = confidence;
+    }
+    
+    public Emotion GetValue()
+    {
+        return new Emotion(Value, Confidence);
+    }
+}
